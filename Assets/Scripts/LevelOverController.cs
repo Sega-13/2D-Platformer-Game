a2a454745c1139bcd8126f45f2b3891da38898b1
Sega-13 <segapatidar@gmail.com>
2024-01-11 14:50:18 +0530

    Assignment- Death After Falling From Platform

--- Assets/Scripts/LevelOverController.cs
+++ Assets/Scripts/LevelOverController.cs
@@ -5,6 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class LevelOverController : MonoBehaviour
 {
+    private Animator anim;
+    private Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Debug.Log("Trpped");
+            Debug.Log("Trpped");
+            Die();
+        }
+    }
+    private void Die()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+        anim.SetTrigger("death");
+    } 
     public void OnTriggerEnter2D(Collider2D collision)
     {
          if(collision.gameObject.GetComponent<PlayerController>() !=null)
@@ -15,5 +36,9 @@ public class LevelOverController : MonoBehaviour
 
     private void CompleteLevel(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
